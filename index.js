4c5e01afc2dd9555a5fbf9f1a7019bae154b576d
hellyeahmalboroknight <terrorpitan@gmail.com>
2025-10-12 15:40:47 +0300

    upg

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-let express = require("express");
+let express = require("express")
+const multer = require("multer")
+const path = require("path")
 
-let ads = [];
+let app = express()
 
-let app = express();
-app.use(express.static("static"));
-app.use(express.json());
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
-app.get("/", (req, res) => res.render("index", {products: ads}));
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, "uploads/")
+    },
+    filename: (req, file, cb)=>{
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({storage})
 
-app.post("/add", (req, res) => {
-    ads.push(req.body);
-    res.send({status: "ok" });
-});
 
-app.use((req, res, next) => {
-    res.status(404);
-    res.render("notFound");
-});
+let ads = []
 
-app.listen(3000, () => console.log("Server on"));
+app.use(express.static("static"))
+app.use(express.json())
+app.set("views", "views")
+app.use(express.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res)=>{
+    res.render("index", {products: ads})
+})
+
+app.get("/post/:id", (req, res)=>{
+    let postId = req.params.id
+    if (!ads[postId]) {
+        res.render("notFound")
+        return
+    }
+    res.render("post", {product: ads[postId]})
+})
+
+app.post("/add", upload.fields([{name: "image"}]), (req, res)=>{
+    let data = {...req.body}
+    console.log(data)
+    data.image = req.files.image.map((file)=>file.filename)
+    data.id = ads.length
+    ads.push(data)
+    res.send({status: "ok"})
+})
+
+app.use((req, res, next)=>{
+    res.status(404)
+    res.render("notFound")
+})
+
+app.listen(3000, ()=>console.log("Server on!"))
--- static/script.js
+++ static/script.js
@@ -1,31 +1,24 @@
-document.querySelector("form").addEventListener("submit", event => {
-    event.preventDefault();
+document.querySelector("form").addEventListener("submit", (event)=>{
+    event.preventDefault()
     fetch("/add", {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            title: event.target["title"].value,
-            desc: event.target["desc"].value,
-        })
-    }).then(() => location.reload())
+        body: new FormData(event.target)
+    }).then(()=>location.reload())
 })
 
-document.querySelector(".closeBtn").addEventListener("click", () => {
-    document.querySelector(".modal").style.display = "none";
+document.querySelector(".closeBtn").addEventListener("click", ()=>{
+    document.querySelector(".modal").style.display = "none"
+})
+document.querySelector("#openBtn").addEventListener("click", ()=>{
+    document.querySelector(".modal").style.display = "grid"
 })
 
-document.querySelector("#openBtn").addEventListener("click", () => {
-    document.querySelector(".modal").style.display = "grid";
-})
-
-// fetch("/ads").then(res => res.json()).then(data => {
-//     console.log(data);
-//     document.querySelector(".wrapper").innerHTML = data.map(el => {
-//         return `<div class="ads">
+// fetch("/ads").then(res=>res.json()).then(data=>{
+//     console.log(data)
+//     document.querySelector(".wrapper").innerHTML = data.map(el=>`
+//         <div class="ads">
 //             <h3>${el.title}</h3>
 //             <p>${el.desc}</p>
-//         </div>`
-//     }).join("");
+//         </div>
+//         `).join("")
 // })
